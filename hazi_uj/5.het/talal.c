10b0204cfee94143eebbb8ecf819309e793a0033
vida-ferenc <70719844+vida-ferenc@users.noreply.github.com>
2026-03-15 20:13:35 +0100

    5.het

---
+++ hazi_uj/5.het/talal.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int benne_van(int n, const int tomb[], int keresett)
+{
+    for (int i = 0; i < n; i++) 
+    {
+        if (tomb[i] == keresett) 
+        {
+            return 1; 
+        }
+    }
+    return 0; 
+}
+
+int main() 
+{
+    int szamok[] = {4, 5, 5557, 72, 641, 1, 625, 72};
+    int meret = 8;
+    int mit_keresünk = 7;
+
+    if (benne_van(meret, szamok, mit_keresünk)) 
+    {
+        printf("Igen, a(z) %d benne van a tombben.\n", mit_keresünk);
+    } 
+    else 
+    {
+        printf("Nem, a(z) %d nincs benne a tombben.\n", mit_keresünk);
+    }
+
+    return 0;
+}
---
+++ hazi_uj/5.het/talalat.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int elso_elofordulas(int n, const int tomb[], int keresett) 
+{
+    for (int i = 0; i < n; i++) 
+    {
+        
+        if (tomb[i] == keresett) 
+        {
+            return i; 
+        }
+    }
+    return -1;
+}
+
+int main() 
+{
+    int szamok[] = {10, 25, 72, 5, 72, 90}; 
+    int meret = 6;
+    int mit_keresunk = 215;
+
+    int index = elso_elofordulas(meret, szamok, mit_keresunk);
+
+    if (index != -1) 
+    {
+        printf("A(z) %d eloszor a(z) % d. indexen található.\n", mit_keresunk, index);
+    } else {
+        printf("A(z) %d nincs benne a tömbben.\n", mit_keresunk);
+    }
+
+    return 0;
+}
